877d85a628eed84522d79a00ec1c9f4dbdbfcf55
darwinn161006 <24022760@vnu.edu.vn>
2025-05-03 23:02:09 +0700

    Thay doi he thong di chuyen
    cu 3 lan xuat hien tang toc 1 lan

--- include(headerfile)/RenderObstacle.hpp
+++ include(headerfile)/RenderObstacle.hpp
@@ -2,18 +2,23 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-struct RenderObstacle
-{
-    float x, y;        // Vị trí của chướng ngại vật
-    int size;          // Kích thước của chướng ngại vật
-    float speed;       // Tốc độ di chuyển
-    SDL_Texture* texture;  // Ảnh của chướng ngại vật
-    SDL_Renderer* renderer; // Thêm renderer vào đối tượng
-
-    // Constructor
+class RenderObstacle {
+public:
     RenderObstacle(float p_x, float p_y, int p_size, SDL_Renderer* p_renderer);
+    ~RenderObstacle();
 
     void update();
     void render();
     bool checkCollision(float characterX, float characterY, int characterW, int characterH);
+    void reset(float p_x, float p_y);
+    float x, y; // Position
+
+private:
+    int size;
+    float baseSpeed;       // Base speed of the obstacle
+    int spawnCount;        // Number of times the obstacle has spawned
+    Uint32 spawnTime;      // Time when the obstacle was spawned
+
+    SDL_Texture* texture;  // Texture for the obstacle
+    SDL_Renderer* renderer;
 };
